2397e6d540c17c543417c589f6e9d09225029f36
TienThanhBN <tienthanhbn2006@gmail.com>
2024-11-29 08:33:52 +0700

    miss

--- bai_5/src/CircleDemo.java
+++ bai_5/src/CircleDemo.java
@@ -1,23 +1,30 @@
-public class AccessModifier {
-    public class Circle {
+public class CircleDemo {
+
+    public static class Circle {
         private double radius = 1.0;
         private String color = "red";
 
         public Circle() {
         }
+
         public Circle(double radius) {
+            if (radius < 0) {
+                throw new IllegalArgumentException("Bán kính không được âm.");
+            }
             this.radius = radius;
         }
+
         public double getRadius() {
             return radius;
         }
+
         public double getArea() {
             return Math.PI * radius * radius;
         }
     }
+
     public static void main(String[] args) {
-        AccessModifier outer = new AccessModifier();
-        Circle circle = outer.new Circle(2.5);
+        Circle circle = new Circle(3.0);
         System.out.println("Radius: " + circle.getRadius());
         System.out.println("Area: " + circle.getArea());
     }
